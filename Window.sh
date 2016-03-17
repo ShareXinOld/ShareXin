447b47d9b85e16837af5aece3932d4c76f1d182a
Bitstick <thebitstick@tfwno.gf>
2016-03-17 06:54:41 -0500

    First commit

---
+++ Window.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# take a screenshot using gnome-screenshot
+image=$(mktemp /tmp/twitter.XXXXXX.png)
+shutter -a --output="$image" -n -e
+
+cp $image ~/Pictures/Screenshots/
+
+export image
+
+# check file size (0 bytes means that gnome-screenshot was cancelled)
+sharenixtmpsize=$(wc -c <"$image")
+if [ $sharenixtmpsize != 0 ]; then
+    xterm -e "python3 /home/thebitstick/Twitter/Window.py"
+fi
