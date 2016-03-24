3015b01a78c5bd274f5143e9b069af56c6b53bbd
Bitstick <thebitstick@tfwno.gf>
2016-03-24 16:18:16 -0500

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sudo apt-get install python3 shutter python3-pip xterm ffmpeg gtk-recordmydesktop
+sudo apt-get install python3 python3-pip ffmpeg gtk-recordmydesktop
 sudo pip3 install Twitter
-sudo pip3 install twitter
