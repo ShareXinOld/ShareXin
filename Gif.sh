840cc78542431bd00e907492d15339aacc9f48d4
Bitstick <thebitstick@tfwno.gf>
2016-03-29 07:21:19 -0500

    More efficient gif copying

--- Gif.sh
+++ Gif.sh
@@ -13,8 +13,8 @@ eval $(slop --opengl)
 # launches ffmpeg and python script
 xfce4-terminal -e "ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -f alsa -i pulse /tmp/sharexin_gif.gif"
 
-# copies gif to permanent location
-cp /tmp/sharexin_gif.gif "${XDG_PICTURES_DIR}/ShareXin/twitter-$date-$time.gif"
-
 # launches python script
 python3 ~/ShareXin/Gif.py
+
+# copies gif to permanent location
+sleep 5 && cp /tmp/sharexin_gif.gif "${XDG_PICTURES_DIR}/ShareXin/twitter-$date-$time.gif"
