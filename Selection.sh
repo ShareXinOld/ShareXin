cda55deb4fbd68b252377a69eb976f45d72782b4
Bitstick <thebitstick@tfwno.gf>
2016-09-15 22:48:51 -0500

    Update Selection.sh

--- Selection.sh
+++ Selection.sh
@@ -3,16 +3,11 @@
 # pictures directory
 XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
 
-# take a screenshot using maim while pausing the screen
+# take a screenshot using gnome-screen while pausing the screen
 gnome-screenshot -a -f /tmp/sharexin_img.png
 
-if [ $? -eq 0 ]
-then
-  # launches python script
-  python3 ~/ShareXin/Picture.py
-else
-  exit 1
-fi
+# launches python script
+python3 ~/ShareXin/Picture.py
 
 # date and time for naming
 date=$(date +%Y-%m-%d)
