d8de6e13d9fe4d4ea6c566c2cb365cca0b7d3907
Bitstick <thebitstick@tfwno.gf>
2016-09-15 22:50:58 -0500

    Update Window.sh

--- Window.sh
+++ Window.sh
@@ -3,8 +3,11 @@
 # pictures directory
 XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
 
-# take a screenshot using maim and xdotool
+# take a screenshot using gnome-screenshot
 gnome-screenshot -w -f /tmp/sharexin_img.png
+
+# launches python script
+python3 ~/ShareXin/Picture.py
 
 # date and time for naming
 date=$(date +%Y-%m-%d)
@@ -12,6 +15,3 @@ time=$(date +%T)
 
 # copies to permanent location
 cp /tmp/sharexin_img.png $XDG_PICTURES_DIR/ShareXin/Window/twitter_window-$date-$time.png
-
-# launches python script
-python3 ~/ShareXin/Picture.py
